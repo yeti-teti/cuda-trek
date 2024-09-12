bd1f9dc6a135de9738524dca28e9f4d4155889de
yet-teti <yetit1762@gmail.com>
2024-09-11 22:38:43 -0700

    Tran-Init

--- cpp/transformers/train_gpt.cpp
+++ cpp/transformers/train_gpt.cpp
@@ -18,22 +18,6 @@
 using namespace std;
 
 // Each Layers forward and Backward passes
-void encoder_forward();
-void encoder_backward();
-void layernorm_forward();
-void layernorm_backward();
-void matmul_forward_naive();
-void matmul_forward();
-void matmul_backward();
-void attention_forward();
-void attention_backward();
-void gelu_forward();
-void gelu_backward();
-void residual_forward();
-void residual_backward();
-void softmax_forward();
-void crossentropy_forward();
-void crossentropy_softmax_backward();
 
 // GPT 2 Definition
 
