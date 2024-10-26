b6ac3ec2c08d3e124af65368cecbeeccc53760e8
yet-teti <yetit1762@gmail.com>
2024-10-25 23:38:54 -0700

    rnn

--- cpp/rnn/train_lstm.cpp
+++ cpp/rnn/train_lstm.cpp
@@ -1,10 +1,61 @@
-#include <iostream>
-#include <math.h>
+// Standard C++ libraries
+#include<iostream>
+#include<math.h>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<vector>
+
 
 using namespace std;
 
-// Input Node
+// Loading the data (WMT-14)
+vector<vector<string> > readCSV(const string& filename){
 
-void iNode() {}
+  ifstream file(filename);
+  vector<vector<string> > data;
+  string line;
 
-int main() { return 0; }
+  if(!file.is_open()){
+    cout<<"Couldn't load data";
+    return data;
+  }
+  
+  while(getline(file, line, '\n')){
+      stringstream ss(line);
+      
+      vector<string> tokens;
+      string token;
+      while(getline(ss, token, ',')){
+        tokens.push_back(token);
+      }
+      data.push_back(tokens);
+  } 
+  file.close();
+
+  return data;
+}
+
+//
+// Model Architecture
+
+
+
+int main() { 
+  
+  // loading the data
+  //string file = "../datasets/wmt-14/wmt14_translate_de-en_train.csv";
+  string file = "../datasets/test.csv";
+  vector<vector<string> > data = readCSV(file);
+
+  for(int i=0;i<data.size();i++){
+    for(string val:data[i]){
+      cout<<val;
+    }
+    cout<<endl;
+  }
+
+
+  return 0; 
+
+}
