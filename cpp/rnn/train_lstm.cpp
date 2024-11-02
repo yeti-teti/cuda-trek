3be84882d3159fa7bbf1e49a3c4a1689a39c6492
yet-teti <yetit1762@gmail.com>
2024-11-01 22:45:07 -0700

    rnn

--- cpp/rnn/train_lstm.cpp
+++ cpp/rnn/train_lstm.cpp
@@ -79,6 +79,33 @@ void dataloader_free(DataLoader *loader){
 
 // --------------------------------------------------------- //
 
+// end-of-text token id
+#define EOT 50256
+
+unsigned int random_u32(unsigned long long *state){
+
+  *state ^= *state >> 12;
+  *state ^= *state << 25;
+  *state ^= *state >> 27;
+  return (*state * 0x2545F4914F6CDD1Dull) >> 32;
+}
+
+float random_f32(unsigned long long *state) { // random float32 in [0,1)
+    return (random_u32(state) >> 8) / 16777216.0f;
+}
+
+int sample_mult(float* probabilities, int n, float coin) {
+    
+    float cdf = 0.0f;
+    for (int i = 0; i < n; i++) {
+        cdf += probabilities[i];
+        if (coin < cdf) {
+            return i;
+        }
+    }
+    return n - 1; 
+}
+
 // Tokenizer (Decoding)
 typedef struct{
   uint32_t vocab_size;
@@ -216,17 +243,39 @@ void lstm_cell_backward(float* dout, float* inp, float* dweight, float* dbias, i
 
 }
 
-void rnn_forward();
-void rnn_backward();
-
 void hadamard();
 
 void sigmoid();
-void tanh();
+void tanh_forward();
 
+void crossentropy_forward(float* losses, float* probs, int* targets, int B, int T, int V){
 
-void crossentropy_forward();
-void crossentropy_backward();
+    for(int b=0;b<B;b++){
+      for(int t=0;t<T;t++){
+        float* probs_bt = probs + b * T * V + t * V;
+        int ix = targets[b * T + t];
+        losses[b * T + t] = -logf(probs_bt[ix]);
+      }
+    }
+}
+void crossentropy_tanh_backward(float* dlogits, float* dlosses, float* probs, int* targets, int B, int T, int V){
+
+  // Backward through both tanh and crossentropy
+  for(int b=0;b<B;b++){
+    for(int t=0;t<T;t++){
+      float* dlogits_bt = dlogits + b * T * V + t * V;
+      float* probs_bt = probs + b * T * V + t * V;
+      float dloss = dlosses[b * T + t];
+
+      int ix = targets[b * T + t];
+      for(int i=0;i<V;i++){
+        float p = probs_bt[i];
+        float indicator = i == ix ? 1.0f : 0.0f;
+        dlogits_bt[i] += (p - indicator) * dloss;
+      }
+    }
+  }
+}
 
 void initalize_weights();
 void gradient_clipping();
@@ -242,7 +291,7 @@ typedef struct{
   float* O; // Output gate
   float* C_tilde; // Input Node
 
-}
+} ParameterTensors;
 
 float* malloc_and_point_parameters(ParameterTensors* params, size_t* param_size){
 
@@ -268,7 +317,7 @@ float* malloc_and_point_parameters(ParameterTensors* params, size_t* param_size)
 
 // TODO: Add all the Activation tensors and change the number
 #define NUM_ACTIVATION_TENSORS 4
-typdef struct{
+typedef struct{
 
 
   float* logits;
@@ -431,9 +480,159 @@ void lstm_forward(LSTM *model, int* inputs, int* targets, int B, int T){
     }
   }
 
-  // Start HERE:
+  // cache the inputs/targets
+  memcpy(model->inputs, inputs, B * T * sizeof(int));
+  if(targets != NULL){
+    memcpy(model->targets, targets, B * T * sizeof(int));
+  } 
+
+  // Forward Pass
+  ParameterTensors params = model->params;
+  ActivationTensors acts = model->acts;
+  float* residual;
+
+  for(int l=0;l<L;l++){
+
+    // Accessing the pointers of the weights for this layer
+    // ...
+
+    // Accessing the pointers of the weights for this layer
+    // ...
+
+    // Forward Pass
+    lstm_cell_forward();    
+  }
+
+  tanh_forward();
+
+  // Forward the cross-entropy loss function is we have the targets
+  if(targets != NULL){
+    crossentropy_forward(model->acts.losses, model->probs, targets, B, T, V);
+
+    // Evaluate the mean loss
+    float mean_loss = 0.0f;
+    for(int i=0;i<B*T;i++){
+      mean_loss += model->acts.losses[i];
+    }
+    mean_loss /= B*T;
+    model->mean_loss = mean_loss;
+  } else{
+    model->mean_loss = -1.0f;
+  }
+}
+
+void lstm_zero_grad(GPT2 *model){
+  if(model->grads_memory != NULL){
+    memset(model->grads_memory, 0, model->num_parameters * sizeof(float));
+  }
+  if(model->grads_acts_memory != NULL){
+    memset(model->grads_acts_memory, 0, model->num_activations * sizeof(float));
+  }
+}
+
+void lstm_backward(LSTM *model){
+
+  // Checking forward pass done with targets
+  if(model->mean_loss == -1.0f){
+    cout<<"Error: Forward pass not done before backward pass"<<endl;
+    exit(1);
+  }
+
+  // Lazily allocate the memory for gradients of the weights and activations, if not done
+  if(model->grads_memory == NULL){
+    model->grads_memory = malloc_and_point_parameters(&model->grads, model->param_sizes);
+    model->grads_acts_memory = malloc_and_point_activations(&model->grads_acts, model->act_sizes);
+    lstm_zero_grad(model);
+  }
+
+  // Params
+  int B = model->batch_size;
+  int T = model->seq_len;
+  int V = model->config.vocab_size;
+  int L = model->config.num_layers;
+  int NH = model->config.num_heads;
+  int C = model->config.channels;
+
+  // Backward Pass, going in reverse order of forward pass and calling backward functions
+  ParameterTensors params = model->params;
+  ParameterTensors grads = model->grads;
+  ActivationTensors acts = model->acts;
+  ActivationTensors grads_acts = model->grads_acts;
+
+  // Starting Chain rule
+  float dloss_mean = 1.0f / (B*T);
+  for(int i=0;i<B*T;i++){
+    grads_acts.losses[i] = dloss_mean;
+  }
+
+  // Start here:
+  crossentropy_tanh_backward(grads_acts.logits, grads_acts.losses, acts.probs, model->targets, B, T, V);
+
+  for(int l=L-1;l>=0;l--){
+
+    // Get pointers of the weights for this layer
+    // ...
+
+    // Get the pointers of the gradients of the weights for this layer
+    // ...
+
+    // Get pointers of the activations for this layer
+    // ... 
+
+    // Get pointers of the gradients of the activations for this layer
+    // ...
+
+    // Backprop Layer
+    lstm_cell_backward();
+  }
 
 }
+
+// Update using AdamW
+void lstm_update(LSTM *model, float learning_rate, float beta1, float beta2, float eps, float weight_decay, int t){
+
+  // Allocate memory for m_memory and v_memory
+  if(model->m_memory == NULL){
+    model->m_memory = (float*)calloc(model->num_parameters, sizeof(float));
+    model->v_memory = (float*)calloc(model->num_parameters, sizeof(float));
+  }
+
+  for(int i=0;i<model->num_parameters;i++){
+    float param = model->params_memory[i];
+    float grad = model->grads_memory[i];
+
+    // Updating the first moment (momentum)
+    float m = beta1 * model->m_memory[i] + (1.0f - beta1) * grad;
+    // Updating the secong moment (RMSprop)
+    float v = beta2 * model->v_memory[i] + (1.0f - beta2) * grad * grad;
+    // Bias-correct both moments
+    float m_hat = m / (1.0f - powf(beta1, t));
+    float v_hat = v / (1.0f - powf(beta2, t));
+
+    // Update
+    model->m_memory[i] = m;
+    model->v_memory[i] = v;
+    model->params_memory[i] -= learning_rate * (m_hat / (sqrtf(v_hat) + eps) + weight_decay * param);
+
+  }
+
+}
+
+void lstm_free(LSTM *model){
+  free(model->params_memory);
+  free(model->grads_memory);
+  free(model->m_memory);
+  free(model->v_memory);
+  free(model->acts_memory);
+  free(model->grads_acts_memory);
+  free(model->inputs);
+  free(model->targets);
+}
+
+
+#ifndef TESTING
+// Skip main below if testing
+
 
 
 int main() { 
@@ -461,15 +660,81 @@ int main() {
 
   Tokenizer tokenizer;
   tokenizer_init(&tokenizer, "../datasets/gpt2_tokenizer.bin");
+  
+
+  // Allocating memory to generate smaples from the model
+  unsigned long long rng_state = 1337;
+  int* gen_tokens = (int*)malloc(B*T*sizeof(int));
+  const inst gentT = 64;
 
   // Train
+  struct timespec start, end;
+  for(int step=0;step<=50;step++){
+
+    // Estimating the validation loss
+    if(step % 10 == 0){
+      float val_loss = 0.0f;
+      dataloader_reset(&val_loader);
+
+      for(int i=0;i<val_num_batches;i++){
+        dataloader_next_batch(&val_loader);
+        lstm_forward(&model, val_loader.inputs, val_loader.targets, B, T);
+        val_loss += model.mean_loss;
+      }
+      val_loss /= val_num_batches;
+      cout<<"Val loss: "<<val_loss<<endl;
+    }
+
+    // Print generated text
+    if(step > 0 && step % 20 == 0){
+      for(int i=0;i<B*T;i++){
+        gen_tokens[i] = TEXT_EOT;
+      }
+      cout<<"Generating:---------"<<endl;
+      for(int t=1;t<genT;t++){
+
+        // Wasteful inference
+        lstm_forward(&model, gen_tokens, NULL, B, T);
+
+        float* probs = model.acts.probs + (t-1) * model.config.vocab_size;
+        float coin = random_32(&rng_state);
+
+        int next_token = sample_mult(probs, model.config.vocab_size, coin);
+        gen_tokens[t] = next_token;
+
+        if(tokenizer.init_ok){
+          const char* token_str = tokenizer_decode(&tokenizer, next_token);
+          safe_printf(token_str);
+        } else{
+          // Printing the token id
+          cout<<next_token;
+        }
+        fflush(stdout);
+      }
+      cout<<"\n-----------\n";
+    }
+
+    // Training step
+    auto start = std::chrono::steady_clock::now();
+    dataloader_next_batch(&train_loader);
+    lstm_forward(&model);
+    lstm_zero_grad(&model);
+    lstm_backward(&model);
+    lstm_update(&model, 1e-4f, 0.9f, 0.999f, 1e-8f, 0.0f, step+1);
+    auto end = std::chrono::steady_clock::now();
+
+    double time_elapsed_s = std::chrono::duration_cast<std::chrono::duration<double>>(end-start).count();
+
+    cout<<"Step: "<<step<<" train loss: "<< model.mean_loss<<" took: "<< time_elapsed_s * 1000;
+  }
 
 
   //Free Memory
   dataloader_free(&train_loader);
   dataloader_free(&val_loader);
   tokenizer_free(&tokenizer);
-  
+  lstm_free(&model);
+  free(gen_tokens);  
 
   return 0; 
 
